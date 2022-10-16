eb3e3f3d4be4192ebf6ba67584227383dac27a48
Yvelchrome <yvelchrome@gmail.com>
2022-10-16 17:25:54 +0200

    feat: html_partial head

---
+++ src/html_partials/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="This is a exercise I had to realize at HETIC, it have for goal to learn how Docker works and get back some PHP basics.">
+  <meta name="author" content="Steven Godin">
+  <link rel="icon" href="../assets/icons/favicon.svg">
+  <title>Docker Exo - Blog</title>
+</head>
